2020c43b16e0163f8531ccb250f9d8b53fbbae59
Brandon Lee <brandonlee2543@hotmail.com>
2019-05-13 13:10:34 -0400

    Implement rudimentary timer functionality

---
+++ scripts/countdown.js
@@ -0,0 +1,29 @@
+document.getElementById('play-button').addEventListener('click', timer);
+
+let countdown;
+const timerDisplay = document.querySelector('.countdown-text');
+
+function timer(seconds) {
+  const now = Date.now();
+  const then = now + seconds * 1000;
+  displayTimeLeft(seconds);
+
+  countdown = setInterval(() => {
+    const secondsLeft = Math.round((then - Date.now()) / 1000);
+    if (secondsLeft < 0) {
+      clearInterval(countdown);
+      return;
+    }
+    displayTimeLeft(secondsLeft);
+  }, 1000);
+}
+
+function displayTimeLeft(seconds) {
+  const minutes = Math.floor(seconds / 60);
+  const remainderSeconds = seconds % 60;
+  const display = `${minutes}:${
+    remainderSeconds < 10 ? '0' : ''
+  }${remainderSeconds}`;
+  document.title = display;
+  timerDisplay.textContent = display;
+}
